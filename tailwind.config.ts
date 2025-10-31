c702bdd70f9c8a332efb02c5ad6559b77bae63f4
rookiecol <juan.diegoam@hotmail.com>
2025-10-30 20:01:35 -0500

    Add Tailwind animation utilities

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,14 +6,27 @@ const config: Config = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-
   ],
   theme: {
     extend: {
       fontFamily: {
         'orbitron': ['Orbitron Variable', 'sans-serif']
-      }
-    }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
